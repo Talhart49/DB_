98fed56d754e1e64807df948d3ed1f5b9f0ade1d
Talhart49 <ranatalhaa01@gmail.com>
2021-06-19 23:45:58 +0500

    GUI

---
+++ src/AddIngredients_D.java
@@ -0,0 +1,112 @@
+import java.awt.*;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class AddIngredients_D extends JFrame {
+
+    private JPanel PaddIngredients;
+    private JButton Addbtn;
+    private JButton backbtn;
+    private JTextField idfld;
+    private JTextField namefld;
+    private JLabel Ingredientslbl;
+
+
+    public static void main(String[] args) {
+        AddIngredients_D frame = new AddIngredients_D();
+        frame.setVisible(true);
+    }
+
+
+    public AddIngredients_D() {
+
+
+        initComponents();
+
+    }
+
+
+
+    private void initComponents() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage("F:\\oop\\ramen.png"));
+        setTitle("Ramen_Here");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1600, 800);
+        PaddIngredients = new JPanel();
+        PaddIngredients.setBackground(new Color(107, 103, 91));
+        setContentPane(PaddIngredients);
+        PaddIngredients.setLayout(null);
+
+        JLabel backgroundlabel = new JLabel("");
+        backgroundlabel.setIcon(new ImageIcon("fp.jpg"));
+        backgroundlabel.setBounds(925, 11, 357, 220);
+        PaddIngredients.add(backgroundlabel);
+
+
+
+
+        JLabel idlbl = new JLabel("ID :");
+        idlbl.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        idlbl.setForeground(new Color(255,255,255));
+        idlbl.setBounds(548, 370, 102, 40);
+        PaddIngredients.add(idlbl);
+
+
+        JLabel namelbl = new JLabel("Name :");
+        namelbl.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        namelbl.setForeground(new Color(255,255,255));
+        namelbl.setBounds(548, 424, 102, 42);
+        PaddIngredients.add(namelbl);
+
+        idfld = new JTextField();
+        idfld.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        idfld.setBounds(714, 370, 283, 44);
+        PaddIngredients.add(idfld);
+        idfld.setColumns(10);
+
+        Addbtn = new JButton("Add");
+        Addbtn.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        Addbtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {}
+        });
+        Addbtn.setBounds(713, 479, 116, 40);
+        PaddIngredients.add(Addbtn);
+
+        backbtn = new JButton("Back");
+        backbtn.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        backbtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+//                dispose();
+//                Admin a=new Admin();
+//                a.setVisible(true);
+
+            }
+        });
+        backbtn.setForeground(new Color(220, 20, 60));
+        backbtn.setBackground(new Color(255, 235, 205));
+        backbtn.setBounds(10, 41, 154, 42);
+        PaddIngredients.add(backbtn);
+
+        namefld = new JTextField();
+        namefld.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        namefld.setColumns(10);
+        namefld.setBounds(714, 424, 283, 44);
+        PaddIngredients.add(namefld);
+
+        Ingredientslbl = new JLabel("Add Ingredients");
+        Ingredientslbl.setForeground(Color.WHITE);
+        Ingredientslbl.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
+        Ingredientslbl.setBounds(728, 254, 210, 40);
+        PaddIngredients.add(Ingredientslbl);
+        setLocationRelativeTo(null);
+
+
+
+    }
+}
+
+
