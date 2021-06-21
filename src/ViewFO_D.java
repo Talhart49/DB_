532bdb492d3e0c07386c7f8e5cd06b383a2d011f
Talhart49 <ranatalhaa01@gmail.com>
2021-06-21 14:09:12 +0500

    GUI

---
+++ src/ViewFO_D.java
@@ -0,0 +1,80 @@
+import java.awt.*;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class ViewFO_D extends JFrame {
+
+    private JPanel PView;
+    private JButton backbtn;
+
+
+    public static void main(String[] args) {
+        ViewFO_D frame = new ViewFO_D();
+        frame.setVisible(true);
+    }
+
+
+    public ViewFO_D() {
+
+
+        initComponents();
+
+    }
+
+
+
+    private void initComponents() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage("F:\\oop\\ramen.png"));
+        setTitle("Ramen_Here");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1600, 800);
+        PView = new JPanel();
+        PView.setBackground(Color.GRAY);
+        setContentPane(PView);
+        PView.setLayout(null);
+
+        JLabel backgroundlabel = new JLabel("");
+        backgroundlabel.setIcon(new ImageIcon("fp.jpg"));
+        backgroundlabel.setBounds(925, 11, 357, 220);
+        PView.add(backgroundlabel);
+
+        backbtn = new JButton("Back");
+        backbtn.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        backbtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+//                dispose();
+//                Admin a=new Admin();
+//                a.setVisible(true);
+
+            }
+        });
+        backbtn.setForeground(new Color(220, 20, 60));
+        backbtn.setBackground(new Color(255, 235, 205));
+        backbtn.setBounds(10, 41, 154, 42);
+        PView.add(backbtn);
+
+        JLabel FOlbl = new JLabel("Food_Order Data");
+        FOlbl.setForeground(Color.WHITE);
+        FOlbl.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
+        FOlbl.setBounds(10, 203, 210, 40);
+        PView.add(FOlbl);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(29, 362, 606, 263);
+        PView.add(scrollPane);
+
+        JTextArea VtextArea = new JTextArea();
+        scrollPane.setViewportView(VtextArea);
+        setLocationRelativeTo(null);
+
+
+
+    }
+}
+
+
